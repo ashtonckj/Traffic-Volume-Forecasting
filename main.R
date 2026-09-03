76684ad0b9c07be015154f6338621b599d17ef3d
ashtonckj <stickmangamer0730@gmail.com>
2026-09-03 23:00:37 +0800

    chore: adds models to main.R

--- main.R
+++ main.R
@@ -5,7 +5,12 @@
 SCRIPTS <- c(
   "src/preprocessing/preprocessing.R",                  # writes data/processed/traffic_volume_processed.csv
   "src/figures/figures_exploratory_data_analysis.R",    # -> output/exploratory/
-  "src/figures/figures_time_series_diagnostics.R"       # -> output/diagnostics/
+  "src/figures/figures_time_series_diagnostics.R",      # -> output/diagnostics/
+  # "src/tuning/tuning_lstm.R",                         # lstm hyperparameters tuning (might take around 8 hours)
+  "src/models/lstm.R",
+  "src/models/sarima.R",
+  "src/models/seasonal_naive.R",
+  "src/models/xg_boost.R",
 )
 
 for (s in SCRIPTS) {
